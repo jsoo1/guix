bd15b41d47bbad9425f8633841049747aa09c65d
John Soo <jsoo1@asu.edu>
2020-04-01 08:23:37 -0700

    gnu: xorg-configuration-directory: Expose the function.
    * gnu/services/xorg.scm (xorg-configuration-directory): Expose it.

--- gnu/services/xorg.scm
+++ gnu/services/xorg.scm
@@ -77,6 +77,7 @@
             xorg-configuration-server
             xorg-configuration-server-arguments
 
+            xorg-configuration-directory
             %default-xorg-modules
             %default-xorg-fonts
             %default-xorg-server-arguments

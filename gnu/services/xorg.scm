8dcc391c148f2009ceacc0b717f0240f96a4b7c1
John Soo <jsoo1@asu.edu>
2020-04-02 03:58:07 -0700

    gnu: %default-xorg-server-arguments: Expose variable.
    * gnu/services/xorg.scm (%default-xorg-server-arguments): Expose it.

--- gnu/services/xorg.scm
+++ gnu/services/xorg.scm
@@ -80,6 +80,7 @@
             xorg-configuration-directory
             xorg-configuration->file
 
+            %default-xorg-server-arguments
             %default-xorg-modules
             %default-xorg-fonts
             %default-xorg-server-arguments

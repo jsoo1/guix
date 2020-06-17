52c0b92a6c9621ad2682b927fac47583e3172efc
John Soo <jsoo1@asu.edu>
2020-06-16 22:41:37 -0700

    gnu: Add rust-rustc-ap-rustc-session-654.0.
    * gnu/packages/crates-io.scm (rust-rustc-ap-rustc-session-654.0): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -22133,6 +22133,46 @@ It is automatically published using the compiler repository at
 @url{https://www.github.com/rust-lang/rust}")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-rustc-ap-rustc-session-654.0
+  (package
+    (name "rust-rustc-ap-rustc-session")
+    (version "654.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "rustc-ap-rustc_session" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "1fcmiylyl37aipg5l3ihhk8bggcyvhw7d14jwhz27mw1395ij6ix"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-getopts" ,rust-getopts-0.2)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-num-cpus" ,rust-num-cpus-1)
+        ("rust-rustc-ap-rustc-ast" ,rust-rustc-ap-rustc-ast-654)
+        ("rust-rustc-ap-rustc-data-structures"
+         ,rust-rustc-ap-rustc-data-structures-654)
+        ("rust-rustc-ap-rustc-errors" ,rust-rustc-ap-rustc-errors-654.0)
+        ("rust-rustc-ap-rustc-feature" ,rust-rustc-ap-rustc-feature-654.0)
+        ("rust-rustc-ap-rustc-fs-util"
+         ,rust-rustc-ap-rustc-fs-util-654.0)
+        ("rust-rustc-ap-serialize"
+         ,rust-rustc-ap-serialize-654)
+        ("rust-rustc-ap-rustc-span" ,rust-rustc-ap-rustc-span-654.0)
+        ("rust-rustc-ap-rustc-target"
+         ,rust-rustc-ap-rustc-target-654.0))))
+    (home-page "https://github.com/alexcrichton/rustc-auto-publish")
+    (synopsis
+     "Automatically published version of compile-time execution in rustc")
+    (description
+     "Use the compile-time execution from the Rust compiler with this crate.
+It is automatically published using the compiler repository at
+@url{https://www.github.com/rust-lang/rust}")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-rustc-ap-rustc-span-654.0
   (package
     (name "rust-rustc-ap-rustc-span")

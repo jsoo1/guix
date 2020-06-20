c0f10952aa2e6b501b1c427f386fe489a1573a6e
John Soo <jsoo1@asu.edu>
2020-06-20 08:50:46 -0700

    gnu: Add rust-rustc-ap-rustc-expand-654.0.
    * gnu/packages/crates-io.scm (rust-rustc-ap-rustc-expane-654.0): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -23572,6 +23572,57 @@ It is automatically published using the compiler repository at
 @url{https://www.github.com/rust-lang/rust}")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-rustc-ap-rustc-expand-654.0
+  (package
+    (name "rust-rustc-ap-rustc-expand")
+    (version "654.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "rustc-ap-rustc-expand" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "007vdd23ir9r1kv958slh1n8gwcfs61abs0f7f9zywm8pisnl1jh"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-log" ,rust-log-0.4)
+        ("rust-rustc-ap-rustc-ast"
+         ,rust-rustc-ap-rustc-ast-654)
+        ("rust-rustc-ap-rustc-ast-passes"
+         ,rust-rustc-ap-rustc-ast-passes-654.0)
+        ("rust-rustc-ap-rustc-ast-pretty"
+         ,rust-rustc-ap-rustc-ast-pretty-654.0)
+        ("rust-rustc-ap-rustc-attr"
+         ,rust-rustc-ap-rustc-attr-654.0)
+        ("rust-rustc-ap-rustc-data-structures"
+         ,rust-rustc-ap-rustc-data-structures-654)
+        ("rust-rustc-ap-rustc-errors"
+         ,rust-rustc-ap-rustc-errors-654.0)
+        ("rust-rustc-ap-rustc-feature"
+         ,rust-rustc-ap-rustc-feature-654.0)
+        ("rust-rustc-ap-rustc-lexer"
+         ,rust-rustc-ap-rustc-lexer-654)
+        ("rust-rustc-ap-rustc-parse"
+         ,rust-rustc-ap-rustc-parse-654.0)
+        ("rust-rustc-ap-serialize"
+         ,rust-rustc-ap-serialize-654)
+        ("rust-rustc-ap-rustc-session"
+         ,rust-rustc-ap-rustc-session-654.0)
+        ("rust-rustc-ap-rustc-span"
+         ,rust-rustc-ap-rustc-span-654.0)
+        ("rust-smallvec" ,rust-smallvec-1))))
+    (home-page "https://github.com/alexcrichton/rustc-auto-publish")
+    (synopsis
+     "Automatically published version of the rustc_expand package used in rustc")
+    (description
+     "Use the Rust macro expansion from the Rust compiler with this crate.
+It is automatically published using the compiler repository at
+@url{https://www.github.com/rust-lang/rust}")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-rustc-ap-rustc-feature-654.0
   (package
     (name "rust-rustc-ap-rustc-feature")
